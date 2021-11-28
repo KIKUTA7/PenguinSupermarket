9bee599969546f9a3450bca23177259449492b8e
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-11-29 03:51:11 +0400

    jhdsbcks

--- src/pgdp/collections/Queue.java
+++ src/pgdp/collections/Queue.java
@@ -1,6 +1,6 @@
 package pgdp.collections;
 
-public interface Queue <T> {
+public interface Queue <T> extends DataStructure {
      void enqueue(T x);
     void dequeue();
 
--- src/pgdp/collections/Stack.java
+++ src/pgdp/collections/Stack.java
@@ -1,6 +1,6 @@
 package pgdp.collections;
 
-public interface Stack <T> {
+public interface Stack <T> extends DataStructure {
     void push(T x);
     void pop();
 
