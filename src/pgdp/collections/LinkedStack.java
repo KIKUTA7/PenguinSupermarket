fb246f8e7f7edb738d4936885e5cd2a22b6e96bd
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-11-29 05:11:58 +0400

    jhdsbcks

--- src/pgdp/collections/LinkedStack.java
+++ src/pgdp/collections/LinkedStack.java
@@ -1,73 +1,31 @@
 package pgdp.collections;
 
 public class LinkedStack<T> implements Stack{
-    private T info;
-    private LinkedStack<T> next;
-    public T getInfo() {
-        return info;
+    private List<T> l;
+    public List<T> getNext() {
+        return l;
     }
-
-    public LinkedStack<T> getNext() {
-        return next;
-    }
-    public LinkedStack(T x)
+    public LinkedStack()
     {
-        info = x;
-        next = null;
+        l = null;
     }
-    public LinkedStack(T x, LinkedStack<T> l) {
-        info = x;
-        next = l;
-    }
-
-
     @Override
     public int size() {
         int count=0;
-        for (LinkedStack<T> t = this; t!=null; t=t.next)
+        for(List<T> t=l;t!=null;t=t.getNext())
             count++;
         return count;
     }
 
     @Override
     public LinkedStack<T> push(Object x) {
-        LinkedStack<T> t =this;
-        while (t.next!=null)
-            t=t.next;
-        t.next  = new LinkedStack<T>((T) x,this);
-        return this;
+         l = new List<T>((T) x,l);
+         return this;
     }
 
     @Override
     public LinkedStack<T> pop() {
-        LinkedStack<T> t =this;
-        while (t.next!=null)
-            t=t.next;
-        t=null;
+        l=l.getNext();
         return this;
-    }
-
-    public void insert(T x) {
-        next = new LinkedStack<>(x, next);
-    }
-
-    public void delete() {
-        if (next != null)
-            next = next.next;
-    }
-
-    public int length() {
-        int result = 1;
-        for (LinkedStack<T> t = next; t != null; t = t.next)
-            result++;
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        String result = "[" + info;
-        for (LinkedStack<T> t = next; t != null; t = t.next)
-            result = result + ", " + t.info;
-        return result + "]";
     }
 }
