cd1025c84f34eb1807fcb769e28ab46f68486736
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-11-29 14:39:20 +0400

    jhdsbcks

---
+++ src/pgdp/collections/LinkedQueue.java
@@ -0,0 +1,61 @@
+package pgdp.collections;
+
+public class LinkedQueue<T> implements Queue<T>{
+    private List<T> first, last;
+    public LinkedQueue()
+    {
+        first=null;
+        last=null;
+    }
+    public List<T> getFirst() {
+        return first;
+    }
+
+    public List<T> getLast() {
+        return last;
+    }
+
+    public void setFirst(List<T> first) {
+        this.first = first;
+    }
+
+    public void setLast(List<T> last) {
+        this.last = last;
+    }
+
+    @Override
+    public int size() {
+        int siz=1;
+        if(first==null && last==null) return 0;
+        for (List<T> t =first;t!=last;t=t.getNext())
+            siz++;
+        return siz;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return first==last && first==null;
+    }
+
+    @Override
+    public void enqueue(T x) {
+           if(first==null && last==null)
+           {
+               first = new List<T>(x);
+               last = first;
+           }
+           else
+           {
+               last.setNext(new List<T> (x));
+               last=last.getNext();
+           }
+
+    }
+
+    @Override
+    public T dequeue() {
+        T ans  = first.getInfo();
+        first = first.getNext();
+        return ans;
+    }
+}
