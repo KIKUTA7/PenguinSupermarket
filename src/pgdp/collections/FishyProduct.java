2d64288649a8dda0607bdc9cf7f7dab68ae9565a
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-12-02 15:14:17 +0400

    jhdsbcks

---
+++ src/pgdp/collections/FishyProduct.java
@@ -0,0 +1,33 @@
+package pgdp.collections;
+
+public class FishyProduct {
+    private final String name;
+    private final int price;
+    public FishyProduct (String name, int price)
+    {
+        if(name==null) {
+            ExceptionUtil.illegalArgument("name==null");
+        }
+        if(price<0) {
+            ExceptionUtil.illegalArgument("price < 0");
+        }
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "FishyProduct{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
