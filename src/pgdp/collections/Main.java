e3653bb8ebf9f097f2cef889ebf77a52faf8fe97
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-12-04 00:08:56 +0400

    jhdsbcks

--- src/pgdp/collections/Main.java
+++ src/pgdp/collections/Main.java
@@ -1,46 +1,45 @@
-package pgdp.collections;
-
-public class Main {
-
-    public static void main(String[] args) {
-//        LinkedStack<Integer> x = new LinkedStack<>();
-//        LinkedQueue<Integer> y = new LinkedQueue<>();
-//        y.enqueue(27);
-//        y.enqueue(8);
-//        y.enqueue(9);
-//        y.dequeue();
-//        y.dequeue();
-//        System.out.println(y.isEmpty());
-//        StackConnector<Integer> x1 = new StackConnector<>(x);
-//        x1.addElement(27);
-//        x1.removeNextElement();
-//        x1.addElement(27);
-//        x1.addElement(27);
-//        x1.addElement(27);
-//        System.out.println(x1.hasNextElement());
-//        DataStructureConnector<Integer> x3 = x1;
-//        DataStructureConnector<Integer> x4 = null;
-//        DataStructureLink<Integer> x5 = new DataStructureLink<>(x3, x4);
-//        x5.moveNextFromAToB();
-//        x5.moveAllFromAToB();
-//        System.out.println(x3.hasNextElement());
-        PenguinCustomer p1 = new PenguinCustomer("mariamI",27);
-        PenguinCustomer p2 = new PenguinCustomer("beqaK",8);
-        Queue<PenguinCustomer> pen = new LinkedQueue<>();
-        pen.enqueue(p1);
-        pen.enqueue(p2);
-        FishyProduct p1cart = new FishyProduct("qafsha",3);
-        FishyProduct p2cart = new FishyProduct("barabulka",7);
-        Queue<FishyProduct> cart1 = new LinkedQueue<>();
-        cart1.enqueue(p1cart);
-        cart1.enqueue(p2cart);
-        cart1.enqueue(p2cart);
-        p1.addProductToBasket(p1cart);
-        p1.addProductToBasket(p2cart);
-        p1.addProductToBasket(p2cart);
-        Checkout check  = new Checkout();
-
-
-
-    }
-}
+//package pgdp.collections;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+////        LinkedStack<Integer> x = new LinkedStack<>();
+////        LinkedQueue<Integer> y = new LinkedQueue<>();
+////        y.enqueue(27);
+////        y.enqueue(8);
+////        y.enqueue(9);
+////        y.dequeue();
+////        y.dequeue();
+////        System.out.println(y.isEmpty());
+////        StackConnector<Integer> x1 = new StackConnector<>(x);
+////        x1.addElement(27);
+////        x1.removeNextElement();
+////        x1.addElement(27);
+////        x1.addElement(27);
+////        x1.addElement(27);
+////        System.out.println(x1.hasNextElement());
+////        DataStructureConnector<Integer> x3 = x1;
+////        DataStructureConnector<Integer> x4 = null;
+////        DataStructureLink<Integer> x5 = new DataStructureLink<>(x3, x4);
+////        x5.moveNextFromAToB();
+////        x5.moveAllFromAToB();
+////        System.out.println(x3.hasNextElement());
+//        PenguinCustomer p1 = new PenguinCustomer("mariamI",27);
+//        PenguinCustomer p2 = new PenguinCustomer("beqaK",8);
+//        Queue<PenguinCustomer> pen = new LinkedQueue<>();
+//        pen.enqueue(p1);
+//        pen.enqueue(p2);
+//        FishyProduct p1cart = new FishyProduct("qafsha",3);
+//        FishyProduct p2cart = new FishyProduct("barabulka",7);
+//        Queue<FishyProduct> cart1 = new LinkedQueue<>();
+//        cart1.enqueue(p1cart);
+//        cart1.enqueue(p2cart);
+//        cart1.enqueue(p2cart);
+//        p1.addProductToBasket(p1cart);
+//        p1.addProductToBasket(p2cart);
+//        p1.addProductToBasket(p2cart);
+//
+//
+//
+//    }
+//}
